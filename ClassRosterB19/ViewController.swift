76256e10347f22c6f542c24600100e2b87f50a6b
lee-leonardo <leoleez@hotmail.com>
2014-07-22 09:56:12 -0700

    With John's and Kirby's explanation, the method call for the loading from the plist is solved! I have to look at optionals and down casting more in detail it seems.

--- ClassRosterB19/ViewController.swift
+++ ClassRosterB19/ViewController.swift
@@ -15,9 +15,9 @@ class ViewController: UIViewController {
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		
-		if classRoster.isEmpty {
-			classRoster = createClassRoster()
-		}
+//		if classRoster.isEmpty {
+//			classRoster = createClassRoster()
+//		}
 	}
 
 	override func didReceiveMemoryWarning() {
@@ -30,36 +30,46 @@ class ViewController: UIViewController {
 		//John Clem, Brad Johnson, Jeff Gayle, Leonardo Lee, Mike Tirenin, Victor Adu, Kirby Shabaga, Collin Atherton, Alex Rice, Dan Hoang
 		
 		//For part 4.
-//		roster.append(Person(firstName: "John", lastName: "Clem"))
-//		roster.append(Person(firstName: "Brad", lastName: "Johnson"))
-//		roster.append(Person(firstName: "Mike", lastName: "Tirenin"))
-//		roster.append(Person(firstName: "Victor", lastName: "Abu"))
-//		roster.append(Person(firstName: "Kirby", lastName: "Shabaga"))
-//		roster.append(Person(firstName: "Collin", lastName: "Atherton"))
-//		roster.append(Person(firstName: "Alex", lastName: "Rice"))
-//		roster.append(Person(firstName: "Dan", lastName: "Hoang"))
-//		
-//		for person in classRoster {
-//			println(person.firstName + " " + person.lastName)
-//		}
+		roster.append(Person(firstName: "John", lastName: "Clem"))
+		roster.append(Person(firstName: "Brad", lastName: "Johnson"))
+		roster.append(Person(firstName: "Mike", lastName: "Tirenin"))
+		roster.append(Person(firstName: "Victor", lastName: "Abu"))
+		roster.append(Person(firstName: "Kirby", lastName: "Shabaga"))
+		roster.append(Person(firstName: "Collin", lastName: "Atherton"))
+		roster.append(Person(firstName: "Alex", lastName: "Rice"))
+		roster.append(Person(firstName: "Dan", lastName: "Hoang"))
 		
+		for person in classRoster {
+			println(person.firstName + " " + person.lastName)
+		}
+		return roster
+	}
+	
+//	Attributed to Kirby Shabaga, explanation by John Clem.
+	func loadRosterFromPlist() -> Array<Person> {
 		//For the challenge.
-		var classRoster = [Person]()
+		var roster = [Person]()
+		
 		let filePath = NSBundle.mainBundle().pathForResource("ClassRoster", ofType: ".plist")
 		let rosterData = NSArray(contentsOfFile: filePath)
 		
-		for var index = 0; index < rosterData.count; index++ {
-			println(rosterData[index])
-			//Didn't work.
-//			var entry = rosterData[index] as Dictionary
-//			var rosterEntry = stringRoster[index]
-//			println("Roster Entry Object: ", rosterEntry)
-//			var firstName = rosterEntry["firstName"]
-//			var lastName = stringRoster[index]["lastName"]
-//			var image = stringRoster[index]["image"]
+//		for var index = 0; index < rosterData.count; index++ {
+//			println(rosterData[index])
+//		}
+		
+		//This next was the hump I needed to traverse. Pretty much it checks if the entry for the NSArray is a Dictionary<String,String>, this puts that into a person object, it is safe (ignoring the as String or the unwrap (just showing both ways it can be done) because of the optional as? statement in the if statement.
+		for entry in rosterData {
+			if let person = entry as? Dictionary<String, String> {
+				//Probably a bug, the other way to do it is the init the objects in the append method with a unwrap.
+				//I wrote both ways to do it (atm) because I feel that it'll be good to look at side by side.
+				let first = person["firstName"] as String
+				let last = person["lastName"]
+				roster.append(Person(firstName: first, lastName: last!))
+			}
 		}
 		
 		return roster
+		
 	}
 
 
